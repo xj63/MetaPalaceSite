400396f0f2d9d31320318c4c94d671caf5dfe336
xj63 <xj63work@163.com>
2025-03-05 19:50:41 +0800

    feat: configure for main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import router from "./router";
 
-createApp(App).mount('#app')
+import "./style.css";
+
+import App from "./App.vue";
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.use(router);
+app.mount("#app");
