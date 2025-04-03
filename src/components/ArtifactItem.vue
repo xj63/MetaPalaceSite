b8ac7e0e2104fc57216106fe03a0dda5a558522d
xj63 <xj63work@163.com>
2025-04-03 23:50:21 +0800

    feat: Make artifact item draggable
    The artifact item container has been made draggable, and the description
    overlay has been made unselectable.

--- src/components/ArtifactItem.vue
+++ src/components/ArtifactItem.vue
@@ -1,17 +1,16 @@
 <template>
-    <div class="group overflow-hidden">
+    <div class="group overflow-hidden" draggable="true">
         <img
             v-if="artifact.imageUrl.length > 0"
             :src="artifact.imageUrl"
             alt="Artifact Image"
             class="w-full h-auto object-cover group-hover:scale-125 duration-200"
-            draggable="true"
             loading="lazy"
             @dragstart="onDragStart"
             @click="viewArtifact"
         />
         <div
-            class="absolute bottom-0 left-0 w-full bg-white/40 dark:bg-black/40 backdrop-blur-md p-4 text-sm text-gray-800 dark:text-gray-200 transform translate-y-full group-hover:translate-y-0 transition-transform duration-200 ease-in-out"
+            class="absolute bottom-0 left-0 w-full bg-white/40 dark:bg-black/40 backdrop-blur-md p-4 text-sm text-gray-800 dark:text-gray-200 transform translate-y-full group-hover:translate-y-0 transition-transform duration-200 ease-in-out select-none"
         >
             {{ artifact.description }}
         </div>
