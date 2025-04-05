f6fd0631b29a0e25a964e0ec95f1ca0167a9db55
xj63 <xj63work@163.com>
2025-04-05 19:32:14 +0800

    chore: Remove unused useRouter import and navigateToSelect function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-import { useRouter } from "vue-router";
 import HomeOne from "@/components/HomeOne.vue";
-
-const router = useRouter();
-const navigateToSelect = () => router.push("/select");
 </script>
 
 <template>
