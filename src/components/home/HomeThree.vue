20d7f80e94b4f5df4b7e3d8aa511f66f5da718bc
xj63 <xj63work@163.com>
2025-04-06 23:30:35 +0800

    fix: full screen three page.

--- src/components/home/HomeThree.vue
+++ src/components/home/HomeThree.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="relative w-full overflow-hidden rounded-lg bg-black/[0.96]">
+    <div
+        class="relative w-full h-full overflow-hidden rounded-lg bg-black/[0.96]"
+    >
         <div
             class="absolute flex w-full flex-col items-center justify-center gap-2 p-8 text-center font-heading text-white"
         >
             <span class="text-4xl font-semibold"> 世界模型 </span>
             <span class="font-sans font-light"> 这里吹点牛 </span>
         </div>
-        <div class="flex">
+        <div class="absolute bottom-0 flex">
             <Spline :scene="sceneUrl" class="mt-24 size-full" />
         </div>
     </div>
